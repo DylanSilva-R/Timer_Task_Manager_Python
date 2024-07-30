ddc837860ea837ef4935965b4c1456b3ad68049e
DylanSilva-R <dylansilvarw@gmail.com>
2024-07-29 23:59:02 -0400

    Started user error handling.

--- GUI_No_Functions/OtherTest.py
+++ GUI_No_Functions/OtherTest.py
@@ -1,10 +1,45 @@
 """
 Experimenting with grid layout.
+
+TODO: Build a scroll bar feature for tasks that the user wants to do.
+TODO: 
+
 """
 import tkinter as tk
+from customException import *
 from tkinter import ttk, font
 
-def createGridSystem(window):
+def checkIfInputIsValid():
+    
+    '''
+        There will be two test cases to check for potential user inputs:
+                First: Check if the characters are integers.
+                Second: Check if the values are less than 0 and not greater than 100
+    '''
+
+    try:
+
+        hoursDigitVal = int(hours.get())
+        minutesDigitVal = int(minutes.get())
+        secondsDigitVal = int(seconds.get())
+        
+        if (hoursDigitVal < 0) or (minutesDigitVal < 0) or (secondsDigitVal < 0):
+            raise customException("One of your values is below 0.")
+        elif (hoursDigitVal > 99) or (minutesDigitVal > 99) or (secondsDigitVal > 99):
+            raise customException("One of our values is greater than 99")
+
+    except customException as ex:
+        print(ex)
+    except ValueError as ex:
+        print("You input the wrong data type")
+
+def restStopWatchValues():
+    
+    
+    return
+
+
+def mainWindowGridSystem(window):
     window.columnconfigure(0, weight = 10)
     window.columnconfigure(1, weight = 1)
     window.columnconfigure(2, weight = 10)
@@ -75,7 +110,7 @@ def createStopWatchContainer(window, style):
     style.configure("innerFrame.TFrame", background="#D9D9D9")
 
     #Create frames
-    stopwatchOuterFrame = ttk.Frame(window, style = "outerFrame.TFrame", width = 200, height = 200)
+    stopwatchOuterFrame = ttk.Frame(window, style = "outerFrame.TFrame", width = 100, height = 100)
     stopwatchOuterFrame.grid(row=1, column=2, padx=10, pady=10, sticky="nsew")
     stopwatchLabel = ttk.Label(stopwatchOuterFrame,
                                text = "Stopwatch",
@@ -110,22 +145,50 @@ def createStopWatchContainer(window, style):
     stopwatchInnerFrame.rowconfigure(2, weight = 1)
     stopwatchInnerFrame.rowconfigure(3, weight = 1)
 
-    #Fit the framForDigits into the stowatch innerFrame
+    #Fit the frameForDigits into the stowatch innerFrame
     frameForDigits = ttk.Frame(stopwatchInnerFrame, style = "outerFrame.TFrame", width = 170, height=100)
     frameForDigits.grid(row = 0, column = 1, columnspan = 2, rowspan= 2)
 
+    #Create grid system for frameForDigits
+    frameForDigits.columnconfigure(0,weight = 1)
+    frameForDigits.columnconfigure(1,weight = 2)
+    frameForDigits.columnconfigure(2,weight = 1)
+    frameForDigits.columnconfigure(3,weight = 2)
+    frameForDigits.columnconfigure(4,weight = 1)
+    frameForDigits.columnconfigure(5,weight = 2)
+    frameForDigits.columnconfigure(6,weight = 1)
+
+    frameForDigits.rowconfigure(0,weight = 1)
+    frameForDigits.rowconfigure(1,weight = 2)
+    frameForDigits.rowconfigure(2,weight = 1)
+
+    #Create label to represent time in frameForDigits. There will be three entry points all seperated by two labels using a colon.
+    hourEntry = ttk.Entry(frameForDigits, textvariable = hours, width = 5)
+    minutesEntry = ttk.Entry(frameForDigits, textvariable = minutes, width = 5)
+    secondsEntry = ttk.Entry(frameForDigits, textvariable = seconds, width = 5)
+
+    colonOne = ttk.Label(frameForDigits, style = "Custom.TLabel", text = ":")
+    colonTwo = ttk.Label(frameForDigits, style = "Custom.TLabel", text = ":")
+
+    #Locate digit entry and colons.
+
+    hourEntry.grid(row = 1, column = 1)
+    minutesEntry.grid(row = 1, column = 3)
+    secondsEntry.grid(row = 1, column = 5)
+
+    colonOne.grid(row = 1, column = 2)
+    colonTwo.grid(row = 1, column = 4)
+
+
     #Create buttons to stop, start, and reset the stopwatch. Locate them in the innerStopwatchFrame
-    startButton = ttk.Button(stopwatchInnerFrame, style = "Custom.TButton", text = "Start")
+    startButton = ttk.Button(stopwatchInnerFrame, style = "Custom.TButton", text = "Start", command = checkIfInputIsValid)
     stopButton = ttk.Button(stopwatchInnerFrame, style = "Custom.TButton", text = "Stop")
     resetButton = ttk.Button(stopwatchInnerFrame, style = "Custom.TButton", text = "Reset")
 
     startButton.grid(row = 2, column = 1)
     stopButton.grid(row = 2, column = 2)
     resetButton.grid(row = 3, column = 1, columnspan = 2)
-    
-    #Create label to represent time in frameForDigits
-    digitsLabel = ttk.Label(frameForDigits, style = "Custom.TLabel", text = "00:00:00") # This label will need to update when functioanlity is implemented.
-    digitsLabel.pack(padx= 50, pady=50)
+
 
 
 if __name__ == "__main__":
@@ -135,6 +198,13 @@ if __name__ == "__main__":
     window['background'] = '#1F1C1C'
     style = ttk.Style()
     style.theme_use('alt')
+
+    hours = tk.StringVar()
+    hours.set("H")
+    minutes = tk.StringVar()
+    minutes.set("M")
+    seconds = tk.StringVar()
+    seconds.set("S")
 
     style.configure('Custom.TLabel', background = '#8B8B8B', foreground = 'white', font = ("inter", 15))
 
@@ -146,7 +216,7 @@ if __name__ == "__main__":
 
     titleLabel.grid(row = 0, column = 0, columnspan = 3, sticky="n")
 
-    createGridSystem(window)
+    mainWindowGridSystem(window)
     createTaskContainer(window, style)
     createStopWatchContainer(window, style)
 
